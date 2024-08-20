a5c1204f6adfb6a798d322a55f64be92320949ea
satheesh <jegatheesh.c@grouppal.in>
2024-08-20 20:22:34 +0530

    Initial Commit

---
+++ APODApp/APODAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  APODAppApp.swift
+//  APODApp
+//
+//  Created by APPLE on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct APODAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APODApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  APODApp
+//
+//  Created by APPLE on 20/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
